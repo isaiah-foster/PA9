825367278587504679cf10e97debeb2302c8eccb
Isaiah Foster <isaiahfoster@gmail.com>
2025-04-15 10:20:54 -0700

    updated player movement

--- PA_9/main.cpp
+++ PA_9/main.cpp
@@ -5,16 +5,17 @@
 int main()
 {
     //window settings
-	sf::VideoMode desktop = sf::VideoMode::getFullscreenModes()[0];
-	unsigned int width = desktop.size.x * 1;
-	unsigned int height = desktop.size.y * 1;
-    sf::RenderWindow window(desktop, "Game", sf::Style::Resize | sf::Style::Close | sf::Style::Titlebar);
+
+    sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+    sf::RenderWindow window(desktop, "Fullscreen Window");
+	float width = desktop.size.x * 1;
+	float height = desktop.size.y * 1;
     sf::Vector2u windowSize = window.getSize();
 
 
     sf::Texture shipTexture;
-    shipTexture.loadFromFile("player_ship.png");
-	player playerShip(shipTexture, { windowSize.x / 2.f, windowSize.y / 2.f });
+    shipTexture.loadFromFile("PlayerShip.png");
+	player playerShip(shipTexture, { width, height});
 
 
     while (window.isOpen())
@@ -23,10 +24,10 @@ int main()
         {
             if (event->is<sf::Event::Closed>())
                 window.close();
-
+			
 
         }
-
+        playerShip.movePlayer(windowSize);
         window.clear();
         window.draw(playerShip);
         window.display();
